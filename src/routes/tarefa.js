2bf8f25104c29322246425aea0a15fc277942ea0
Eric Edward Kunzel <kunzel.eric@gmail.com>
2023-04-05 08:24:02 -0300

    Edição de tarefas implementado

--- src/routes/tarefa.js
+++ src/routes/tarefa.js
@@ -23,5 +23,33 @@ router.post('/criar', authUser, connectDataBase, async function (req, res) {
   }
 });
 
+router.put('/editar/:id', authUser, connectDataBase, async function (req, res) {
+  try {
+    // #swagger.tags = ['Tarefa']
+    let idTarefa = req.params.id; //coleta o ID da URL
+    let { posicao, titulo, descricao, status, dataEntrega } = req.body;
+    const usuarioLogado = req.usuarioJwt.id;
+
+    const checkTarefa = await EsquemaTarefa.findOne({ _id: idTarefa, usuarioCriador: usuarioLogado });
+    if(!checkTarefa){
+      throw new Error("Tarefa não encontrada ou pertence a outro usuário!")
+    }
+
+    const tarefaAtualizada = await EsquemaTarefa.updateOne({ _id: idTarefa }, { posicao, titulo, descricao, status, dataEntrega });
+    if(tarefaAtualizada?.modifiedCount>0){
+      const dadosTarefa = await EsquemaTarefa.findOne({ _id: idTarefa }).populate('usuarioCriador');
+
+      res.status(200).json({
+        status: "OK",
+        statusMessage: "Tarefa atualizada com sucesso.",
+        response: dadosTarefa
+      })
+    }
+
+  } catch (error) {
+    return tratarErrosEsperados(res, error);
+  }
+});
+
 
 module.exports = router;
